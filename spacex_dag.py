ac96672cb67e06d577e9f4acb7a5d3fdf69c9b06
Aleksandr Shanin <44789630+Funsy@users.noreply.github.com>
2021-03-01 01:58:21 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -21,14 +21,14 @@ with dag:
     for i in rocket_list:
         t1 = BashOperator(
             task_id="get_data",
-            bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data -r {}".format(i),
+            bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data -r {}".format('' if i == 'all' else i),
             dag=dag
         )
 
         t2 = BashOperator(
             task_id="print_data",
             bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv",
-            params={"rocket": i}, # falcon1/falcon9/falconheavy
+            params={"rocket": '' if i == 'all' else i}, # falcon1/falcon9/falconheavy
             dag=dag
         )
 
